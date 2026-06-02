72d9ab331153f6dde09c3ea1232c4d50b58b09c1
basmahmasood <basmah.masood@gmail.com>
2026-06-02 13:56:20 -0400

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,19 @@
+html,
+body {
+  margin: 0;
+  padding: 0;
+  background: #000;
+  font-family: "Instrument Sans", sans-serif;
+}
+
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+canvas {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
